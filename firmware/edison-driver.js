e5f8b71d406428c93459f714ddd8b3b22eb268cc
Joe Ferner <joe@fernsroth.com>
2015-11-28 22:36:40 -0500

    initial

--- firmware/edison-driver.js
+++ firmware/edison-driver.js
@@ -1,6 +1,6 @@
 
-
 (function() {
+  this.name = 'Motor Controller';
   this.clientJsUrl = 'https://raw.githubusercontent.com/joeferner/edison-motor-controller-bulb/master/firmware/edison-client.js';
 
   this.getBlocklyToolboxData = function() {
