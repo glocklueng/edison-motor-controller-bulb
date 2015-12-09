51e92cf2bac86f55e119299855a487242a8b31da
Joe Ferner <joe@fernsroth.com>
2015-12-09 08:59:35 -0500

    use HAL_FLASHEx_Erase

--- bootloader/src/edison-motor-controller-bootloader.c
+++ bootloader/src/edison-motor-controller-bootloader.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stm32l0xx_hal_flash.h>
+#include <stm32l0xx_hal_flash_ex.h>
 #include <stm32l0xx_hal_spi.h>
 #include <core_cmFunc.h>
 
-#define APP_ADDRESS  0x08002000
+#define APP_ADDRESS  (FLASH_BASE + 0x3000)
 #define SPI          (&hspi1)
 #define SIGNATURE    0x12345678
-#define PAGE_SIZE    ((uint32_t)0x00000080)
+#define FLASH_PAGE_SIZE    ((uint32_t)0x00000080)
 #define WORD_SIZE    4
 #define TIMEOUT      10000
 
@@ -25,9 +26,8 @@ typedef struct {
 
 extern SPI_HandleTypeDef hspi1;
 FlashHeader flashHeader;
-uint8_t buffer[PAGE_SIZE];
+uint8_t buffer[FLASH_PAGE_SIZE];
 uint8_t spiState;
-uint8_t* txData, *rxData;
 uint32_t pageAddr;
 
 void jumpToUserCode();
@@ -37,6 +37,7 @@ void spi_process();
 void spi_beginReceiveStartPage();
 void spi_beginReceivePage();
 void spi_flashPage();
+void eraseProgramMemory();
 
 void setup() {
   deassertIrq();
@@ -44,14 +45,10 @@ void setup() {
   while (HAL_GPIO_ReadPin(PIN_SPICS_PORT, PIN_SPICS_PIN) != GPIO_PIN_RESET);
 
   spiState = SPI_STATE_RECV_HEADER;
-  txData = rxData = (uint8_t*)&flashHeader;
-  memset(txData, 0, sizeof(FlashHeader));
-  for(int i=0; i<sizeof(FlashHeader); i++) {
-    txData[i] = i;
-  }
-  if (HAL_SPI_TransmitReceive_DMA(SPI, txData, rxData, sizeof(FlashHeader)) != HAL_OK) {
-    jumpToUserCode();
-    return;
+  uint8_t* rxData = (uint8_t*)&flashHeader;
+  memset(rxData, 0, sizeof(FlashHeader));
+  if (HAL_SPI_TransmitReceive_DMA(SPI, rxData, rxData, sizeof(FlashHeader)) != HAL_OK) {
+    while (1);
   }
 }
 
@@ -72,67 +69,74 @@ void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef* hspi) {
 }
 
 void HAL_SPI_ErrorCallback(SPI_HandleTypeDef* hspi) {
-  jumpToUserCode();
+  while (1);
 }
 
 void spi_process() {
-  switch(spiState) {
-    case SPI_STATE_RECV_HEADER:
-      if(flashHeader.signature != SIGNATURE) {
-        jumpToUserCode();
-        return;
-      }
-      txData = rxData = (uint8_t*)buffer;
-      HAL_FLASH_Unlock();
-      for(pageAddr = 0; pageAddr < flashHeader.size; pageAddr += PAGE_SIZE) {
-	FLASH_Erase_Page(APP_ADDRESS + pageAddr);
-      }
-      pageAddr = 0;
-      spi_beginReceiveStartPage();
-      break;
-      
-    case SPI_STATE_RECEIVE_START_PAGE:
-      spi_beginReceivePage();
-      break;
-      
-    case SPI_STATE_RECEIVE_PAGE:
-      spi_flashPage();
-      spi_beginReceiveStartPage();
-      break;
+  switch (spiState) {
+  case SPI_STATE_RECV_HEADER:
+    if (flashHeader.signature != SIGNATURE) {
+      while (1);
+    }
+    eraseProgramMemory();
+    pageAddr = 0;
+    spi_beginReceiveStartPage();
+    break;
+
+  case SPI_STATE_RECEIVE_START_PAGE:
+    spi_beginReceivePage();
+    break;
+
+  case SPI_STATE_RECEIVE_PAGE:
+    spi_flashPage();
+    spi_beginReceiveStartPage();
+    break;
+  }
+}
+
+void eraseProgramMemory() {
+  FLASH_EraseInitTypeDef eraseInitStruct;
+  uint32_t pageError;
+
+  HAL_FLASH_Unlock();
+
+  eraseInitStruct.TypeErase = TYPEERASE_PAGEERASE;
+  eraseInitStruct.Page = APP_ADDRESS;
+  eraseInitStruct.NbPages = flashHeader.size / FLASH_PAGE_SIZE;
+  if (HAL_FLASHEx_Erase(&eraseInitStruct, &pageError) != HAL_OK) {
+    while (1);
+  }
+  if (FLASH_WaitForLastOperation(TIMEOUT) != HAL_OK) {
+    while (1);
   }
 }
 
 void spi_beginReceiveStartPage() {
   spiState = SPI_STATE_RECEIVE_START_PAGE;
-  *((uint32_t*)txData) = pageAddr;
-  assertIrq();
-  if (HAL_SPI_TransmitReceive_DMA(SPI, txData, rxData, 4) != HAL_OK) {
-    jumpToUserCode();
-    return;
+  *((uint32_t*)buffer) = pageAddr;
+  if (HAL_SPI_TransmitReceive_DMA(SPI, buffer, buffer, 4) != HAL_OK) {
+    while (1);
   }
+  assertIrq(); // this needs to come after starting the DMA to ensure the DMA is ready to accept bytes
 }
 
 void spi_beginReceivePage() {
   spiState = SPI_STATE_RECEIVE_PAGE;
-  memset(txData, 0, PAGE_SIZE);
-  for(int i=0; i<PAGE_SIZE; i++) {
-    txData[i] = i;
+  memset(buffer, 0, FLASH_PAGE_SIZE);
+  if (HAL_SPI_TransmitReceive_DMA(SPI, buffer, buffer, FLASH_PAGE_SIZE) != HAL_OK) {
+    while (1);
   }
-  deassertIrq();
-  if (HAL_SPI_TransmitReceive_DMA(SPI, txData, rxData, PAGE_SIZE) != HAL_OK) {
-    jumpToUserCode();
-    return;
-  }
+  deassertIrq(); // this needs to come after starting the DMA to ensure the DMA is ready to accept bytes
 }
 
 void spi_flashPage() {
-  for (uint32_t pageOffset = 0; pageOffset < PAGE_SIZE; pageOffset += WORD_SIZE) {
-    if (HAL_FLASH_Program(TYPEPROGRAM_WORD, APP_ADDRESS + pageAddr + pageOffset, *((uint32_t*)(rxData + pageOffset))) != HAL_OK) {
-      jumpToUserCode();
-      return;
+  for (uint32_t pageOffset = 0; pageOffset < FLASH_PAGE_SIZE; pageOffset += WORD_SIZE) {
+    uint32_t word = *((uint32_t*)(&buffer[pageOffset]));
+    if (HAL_FLASH_Program(TYPEPROGRAM_WORD, APP_ADDRESS + pageAddr + pageOffset, word) != HAL_OK) {
+      while (1);
     }
   }
-  pageAddr += PAGE_SIZE;
+  pageAddr += FLASH_PAGE_SIZE;
 }
 
 void assertIrq() {
